a436de94394a976f74d71d48153c4d1700bdaafa
Patrick Saade <git@cybert.rip>
2020-09-20 20:53:22 -0700

    Updating GUI
    Added an interactable GUI to set parameter of generated image. Currently a mess but still a work in progress.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # Pull Function from app.py and draw.py
 from app import start, drawImage
+from draw import *
+from gui import *
+from gui_support import *
 
-# Create Image
-drawImage(512, 512, 1)
 
 # Start app
+vp_start_gui()
+print(gui_support.generateButton.get())
+
+# Create Image
+length = int(gui_support.lengthInput.get())
+width = int(gui_support.widthInput.get())
+
+drawImage(width, length, 1)
+
 start()
